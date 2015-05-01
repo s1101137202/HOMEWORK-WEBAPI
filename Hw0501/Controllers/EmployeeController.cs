55925081350d9976da0bd79ad921502c4ea46814
s1101137202 <dodokey@hotmail.com.tw>
2015-05-01 22:35:46 +0800

    123123123

---
+++ Hw0501/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Hw0501.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpPost]
+        public Employee AddEmployee(Employee employee)
+        {
+            CheckEmployeeIsNotNullThrowException(employee);
+
+            try
+            {
+                EmployeeService.AddEmployee(employee);
+                return EmployeeService.GetEmployeeById(employee.Id);
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private void CheckEmployeeIsNotNullThrowException(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Hw0501/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hw0501.WebApiApplication" Language="C#" %>
